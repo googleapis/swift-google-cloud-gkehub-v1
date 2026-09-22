6a1e783c8b99c9290126880c5254bc7e40979c88
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudGKEHubV1/RBACRoleBinding.swift
+++ Sources/GoogleCloudGKEHubV1/RBACRoleBinding.swift
@@ -239,6 +239,12 @@ public struct RBACRoleBinding: Codable, Equatable, GoogleWKT._AnyPackable,
     }
 
     /// PredefinedRoles is an ENUM representation of the default Kubernetes Roles
+    ///
+    /// - Note: Adding cases to this enumeration is not considered a breaking change.
+    ///   Always include an `@unknown default:` case when switching over this type.
+    ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+    ///   expecting specific values to remain unparsed; future releases may promote
+    ///   them to named cases.
     public enum PredefinedRoles: Codable, Equatable, Sendable {
       /// UNKNOWN
       case unknown
@@ -253,15 +259,21 @@ public struct RBACRoleBinding: Codable, Equatable, GoogleWKT._AnyPackable,
       case anthosSupport
       /// Encodes an unknown integer value.
       ///
-      /// The most common cause for an unknown values is for the service to send
+      /// The most common cause for an unknown value is for the service to send
       /// a value unknown to the library. We recommend you update your library to
       /// the latest version.
+      ///
+      /// - Warning: Do not pattern-match specific integer values in this case;
+      ///   future releases may promote them to named enum cases.
       case unknownIntValue(Int)
       /// Encodes an unknown string value.
       ///
-      /// The most common cause for an unknown values is for the service to send
+      /// The most common cause for an unknown value is for the service to send
       /// a value unknown to the library. We recommend you update your library to
       /// the latest version.
+      ///
+      /// - Warning: Do not pattern-match specific string literals in this case;
+      ///   future releases may promote them to named enum cases.
       case unknownStringValue(String)
 
       public init() {
--- Sources/GoogleCloudGKEHubV1/SecurityPostureConfig.swift
+++ Sources/GoogleCloudGKEHubV1/SecurityPostureConfig.swift
@@ -88,6 +88,12 @@ public struct SecurityPostureConfig: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// Mode defines enablement mode for GKE Security posture features.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum Mode: Codable, Equatable, Sendable {
     /// Default value not specified.
     case unspecified
@@ -99,15 +105,21 @@ public struct SecurityPostureConfig: Codable, Equatable, GoogleWKT._AnyPackable,
     case enterprise
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
@@ -200,6 +212,12 @@ public struct SecurityPostureConfig: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// VulnerabilityMode defines enablement mode for vulnerability scanning.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum VulnerabilityMode: Codable, Equatable, Sendable {
     /// Default value not specified.
     case unspecified
@@ -212,15 +230,21 @@ public struct SecurityPostureConfig: Codable, Equatable, GoogleWKT._AnyPackable,
     case vulnerabilityEnterprise
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
