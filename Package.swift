fce0f42e17013be5010170076bbb17c937794ba4
Jeff Ching <chingor@google.com>
2026-09-23 20:13:22 +0000

    chore: release circa 2026-09-23 (#1099)
    Bump library versions to 0.3.0 and regenerate client libraries for
    release.

--- Package.swift
+++ Package.swift
@@ -27,42 +27,42 @@ let package = Package(
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-auth",
       path: "pkgs/swift-google-auth",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-cloud-gkehub-configmanagement-v1",
       path: "generated/swift-google-cloud-gkehub-configmanagement-v1",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-cloud-gkehub-multiclusteringress-v1",
       path: "generated/swift-google-cloud-gkehub-multiclusteringress-v1",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-cloud-gkehub-rbacrolebindingactuation-v1",
       path: "generated/swift-google-cloud-gkehub-rbacrolebindingactuation-v1",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-gax",
       path: "pkgs/swift-google-gax",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-longrunning",
       path: "generated/swift-google-longrunning",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-rpc",
       path: "generated/swift-google-rpc",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-wkt",
       path: "pkgs/swift-google-wkt",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     .package(url: "https://github.com/apple/swift-log", from: "1.12.0"),
   ],
